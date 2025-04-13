8e351960b7cfe7f61c16ebffa41e9c1eed1e79b7
mafigueroa6 <martiin.aw.9329@gmail.com>
2025-04-13 19:32:27 -0400

    "9 of x"

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -34,6 +34,7 @@ ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
+mkdir data/processed
 mkdir data/processed/server_logs
 mkdir data/processed/user_logs
 mkdir data/processed/event_logs
