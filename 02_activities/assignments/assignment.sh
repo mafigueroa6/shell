81690d465817647eb7646cd6f0c2166282250186
mafigueroa6 <martiin.aw.9329@gmail.com>
2025-04-13 18:59:41 -0400

    "7th"

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -27,7 +27,7 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 
 mkdir data/raw
-mv rawdata data/raw
+mv rawdata/* data/raw
 
 # 3. List the contents of the ./data/raw directory
 
@@ -42,12 +42,12 @@ mkdir data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
-cp ./data/raw/*server*.log/* data/processed/server_logs
+cp data/raw/*server*.log data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
-cp ./data/raw/*user*.log/* data/processed/user_logs
-cp ./data/raw/*event*.log/* data/processed/event_logs
+cp data/raw/*user*.log data/processed/user_logs
+cp data/raw/*event*.log data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
